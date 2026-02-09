3b5611ef7d04dae4e60e3ef82456ab2a20fa6b00
DWandless <drew.wandless@dxc.com>
2026-02-09 16:51:40 +0000

    added fix to level 2 finish line, need to make sure level 2 GBFS + A* make it around the track but dont win. Need to ensure game functions for level 3 & 4 also.

--- resources.py
+++ resources.py
@@ -55,7 +55,7 @@ FPS = 60
 GRID_SIZE = 4
 CHECKPOINT_RADIUS = 45 # Increased by 5 pixels for easier checkpoint detection
 DEBUG_SHOW_CHECKPOINTS = True  # Set to True to show red checkpoint dots and pathfinding visualization
-DEBUG_UNLOCK_ALL_LEVELS = False  # Set to True to unlock all levels for testing/debugging
+DEBUG_UNLOCK_ALL_LEVELS = True  # Set to True to unlock all levels for testing/debugging
 
 # --------------------------------------------------
 # Static assets
@@ -306,7 +306,7 @@ def load_track_for_level(level):
         track_img = "assets/track2.png"
         border_img = "assets/track_border2.png"
 
-        FINISH_POSITION = (20, 350)
+        FINISH_POSITION = (30, 420)
         START_POSITION = (70, 288)
 
         
